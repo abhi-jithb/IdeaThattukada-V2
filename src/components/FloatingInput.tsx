7d30c55cb41a8db865c31b5d9c957a30a7007e85
Abhijith B <babhijith58@gmail.com>
2026-05-28 15:17:17 +0530

    feat: render dynamic thoughts list

--- src/components/FloatingInput.tsx
+++ src/components/FloatingInput.tsx
@@ -2,17 +2,32 @@ import { useState } from "react";
 
 function FloatingInput(){
     const [thought, setThought] = useState("");
+    const [thoughts, setThoughts] = useState<string[]>([]);
+
     const handleAddThought = () => {
-        console.log(thought);
+        if(!thought.trim()) return;
+
+        setThoughts([thought, ...thoughts])
         setThought("");
+
+        console.log(thought);
     }
 
     return(
-        <div className="floating-input">
-            <input type="text" placeholder="Throw that thought here" value={thought} onChange={(e) => setThought(e.target.value)}/>
-            <button onClick={handleAddThought}>Add</button>    
-        </div>
+        <>
+            <div className="floating-input">
+                <input type="text" placeholder="Throw that thought here" value={thought} onChange={(e) => setThought(e.target.value)}/>
+                <button onClick={handleAddThought}>Add</button>    
+            </div>
+
+            <div className="thought-list">
+                {thoughts.map((item, index) => (
+                    <div className="thought-card" key={index}>{item}</div> 
+                ))}
+                
+            </div> 
+        </>
     );
 }
 
-export default FloatingInput
+export default FloatingInput;
