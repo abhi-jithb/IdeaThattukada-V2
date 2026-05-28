6b339d15975531844589252c71057418e35ddb58
Abhijith B <babhijith58@gmail.com>
2026-05-28 14:19:10 +0530

    feat: add controlled floating input state

--- src/components/FloatingInput.tsx
+++ src/components/FloatingInput.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 function FloatingInput(){
+    const [thoughts, setThought] = useState("");
+
     return(
         <div className="floating-input">
-            <input type="text" placeholder="Throw that thought here" />
-            <button>Add</button>
+            <input type="text" placeholder="Throw that thought here" value={thoughts} onChange={(e) => setThought(e.target.value)}/>
+            <button>Add</button>    
         </div>
     );
 }
