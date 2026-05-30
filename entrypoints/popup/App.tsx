e93d95e910a8eec26825ffa50b207ff6c94de606
Abhijith B <babhijith58@gmail.com>
2026-05-31 00:59:55 +0530

    refactor: lift thoughts state to app component

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,22 +1,36 @@
 import "./App.css"
+import  {useState} from 'react'
 import Header from "../../src/components/Header"
 import EmptyState from "../../src/components/EmptyState"
 import FloatingInput from "../../src/components/FloatingInput"
 
 function App(){
+  const [thoughts, setThoughts] = useState<string[]>([])
+  const addThought = (newThought: string) => {
+    setThoughts([newThought, ...thoughts]);
+  };
+
   return(
     <main className="container">
       <Header 
-        title="Idea Thattukada V2"
+        title="Idea Thattukada V2"  
         subtitle="Stack poping up ideas in a nest without loosing"
         />
         
-      <FloatingInput/>
+      <FloatingInput onAddThought={addThought} />
 
-      <EmptyState 
+      <div className="thought-list">
+          {thoughts.map((item, index) => (
+            <div className="thought-card" key={index}>{item}</div> 
+          ))}
+      </div> 
+
+      {thoughts.length === 0 && (
+        <EmptyState 
         title = "No Project YET"
         subtitle = "Project NoteBook will appear here.."
       />
+      )}
 
     </main>
   )
--- src/components/FloatingInput.tsx
+++ src/components/FloatingInput.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 
-function FloatingInput(){
-    const [thought, setThought] = useState("");
-    const [thoughts, setThoughts] = useState<string[]>([]);
+type FloatingInputProps = {
+    onAddThought: (newThought: string) => void;
+};
 
+function FloatingInput({onAddThought}: FloatingInputProps)
+{
+    const [thought, setThought] = useState("");
     const handleAddThought = () => {
         if(!thought.trim()) return;
 
-        setThoughts([thought, ...thoughts])
+        onAddThought(thought);
         setThought("");
 
         console.log(thought);
@@ -19,13 +22,6 @@ function FloatingInput(){
                 <input type="text" placeholder="Throw that thought here" value={thought} onChange={(e) => setThought(e.target.value)}/>
                 <button onClick={handleAddThought}>Add</button>    
             </div>
-
-            <div className="thought-list">
-                {thoughts.map((item, index) => (
-                    <div className="thought-card" key={index}>{item}</div> 
-                ))}
-                
-            </div> 
         </>
     );
 }
